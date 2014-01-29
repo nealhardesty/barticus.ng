a00863447a2b9a79d7b7237148d8e1e8c073d4a3
nhardesty <nhardesty@salesforce.com>
2014-01-28 18:48:05 -0800

    temp

--- js/indexController.js
+++ js/indexController.js
@@ -3,12 +3,26 @@ var barticusApp = angular.module('barticusApp', []);
 //http://www.w3schools.com/html/html5_geolocation.asp
 
 var indexController = function ($scope){
-    $scope.foo="nein";
     navigator.geolocation.getCurrentPosition(function(data) {
         $scope.$apply(function() {
             $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V", false);
+            xmlhttp.send();
+            $scope.statusText = xmlhttp.statusText;
+            $scope.stations = xmlhttp.responseText;
+            var xml = xmlhttp.responseXML;
+            $scope.stationnames = [];
+            for(var i in xml.getElementsByTagName("name")) {
+                $scope.stationnames[i] = xml.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+            }
+           // $scope.stationnames = xml.getElementsByTagName("name");
+            $scope.stationname = xml.getElementsByTagName("name")[0];
+            //debugger;
+            console.log(xml.getElementsByTagName("name")[0]);
+
         });
     }, function(errdata) {
-        alert("narp " + errdata.code);
+        alert("narp getting geo " + errdata.code);
     });
 };
